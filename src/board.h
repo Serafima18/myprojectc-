1233053dfd5409dce946981525bbb860cc2388a1
Ksenia Sharipova <126398622+Serafima18@users.noreply.github.com>
2024-12-26 07:58:12 +0300

    Изменен файл board.h

--- src/board.h
+++ src/board.h
@@ -1,28 +1,17 @@
-#ifndef CHECKERS_BOARD_H
-#define CHECKERS_BOARD_H
+#ifndef BOARD_H
+#define BOARD_H
 
-#include "C:/Users/89853/Desktop/Git/myprojectc++/FLTK/include/FL/Fl_Widget.H"
 #include <vector>
 #include "piece.h"
 
-class Board : public Fl_Widget {
+class Board {
 public:
-    Board(int x, int y, int w, int h);
-    void draw() override;
-    int handle(int event) override;
-
+    Board();
+    void draw();
+    void initializePieces();
+    std::vector<Piece>& getPieces();
 private:
-    void drawPiece(int row, int col, const Piece& piece) const;
-    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol) const;
-    bool canCapture(int fromRow, int fromCol, int toRow, int toCol) const;
-    void movePiece(int fromRow, int fromCol, int toRow, int toCol);
-    void selectPiece(int row, int col);
-    void unselectPiece();
-
-    int size;
-    int selectedRow, selectedCol;
-    bool pieceSelected;
-    std::vector<std::vector<Piece>> board;
+    std::vector<Piece> pieces;
 };
 
-#endif // CHECKERS_BOARD_H
+#endif
